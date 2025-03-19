479f06835e7aac98dd44b34611a04f6a34f1c850
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2025-03-19 19:46:01 +0530

    Googlr Authentication completed.

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import "../CSS/Signup.css";
 import { FcGoogle } from "react-icons/fc";
+import { app } from "../firebase";
+import { useDispatch } from 'react-redux';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../Redux/Slice/userSlice";
+import axios from 'axios';
 
 const OAuth = () => {
+  const auth = getAuth(app);
+  const dispatch = useDispatch();  
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    dispatch(signInStart())
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const userData = { 
+        name: result.user.displayName,
+        email: result.user.email,
+        profilePic: result.user.photoURL,}
+     const data = await axios.post("http://localhost:5000/api/auth/google",userData)
+
+        localStorage.setItem('Token',data.token)
+        dispatch(signInSuccess(data));
+        navigate("/");
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+
     return (
         <div className="text-center">
-        <button className="btn google-btn w-100 d-flex justify-content-center align-items-center">
+        <button className="btn google-btn w-100 d-flex justify-content-center align-items-center" onClick={handleSubmit}>
             <FcGoogle size={20} />
           <i className="bi bi-google ms-2"></i> Continue with Google
         </button>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "tastytrove-e7823.firebaseapp.com",
+  projectId: "tastytrove-e7823",
+  storageBucket: "tastytrove-e7823.firebasestorage.app",
+  messagingSenderId: "996381415903",
+  appId: "1:996381415903:web:1492ccf8a0962ad955cc6a"
+};
+console.log("API Key: ",import.meta.env.VITE_FIREBASE_API_KEY)
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
